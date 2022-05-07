4f9158e879c51fb57623c01e07b2f3543e9ead25
[Github <tmddms313@naver.com>
2022-05-08 01:16:38 +0900

    seccond

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import "./App.css";
-import Hello from "./component/Hello";
-import Welcome from "./component/Welcome";
+import './index.css';
+import Day from './component/Day';
+import DayList from './component/DayList';
+import Header from './component/Header';
+import { BrowserRouter, Route , Routes } from 'react-router-dom';
+import EmptyPage from './component/EmptyPage';
 
 function App() {
-  return (
-    <div className="App">
-      <Hello />
-      <Hello />
-      <Hello />
-      <Welcome />
-    </div>
+  return ( 
+      <div className='App'>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList/>} />
+          <Route path="/day/:day" element={<Day/>} />
+          <Route path="*" element={<EmptyPage/>} />
+        </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
 
